4305f1fe0c1a296349d1f5ec207826f845c73257
docscodes <docscodes@gmail.com>
2024-09-15 14:05:06 -0400

    styling the search page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,13 @@ export default function Search() {
       <Head>
         <title>WeatherWise</title>
       </Head>
-      <main>
-        <h1>WeatherWise</h1>
-        <form >
-          <h2>Search for local weather</h2>
-          <SearchBox />
+      <main className="mt-5 mx-5">
+        <h1 className="text-xl font-medium mb-4">WeatherWise</h1>
+        <form>
+          <h2 className="text-lg mb-4">Search for local weather</h2>
+          <div className="mb-4">
+            <SearchBox />
+          </div>
         </form>
       </main>
     </>
